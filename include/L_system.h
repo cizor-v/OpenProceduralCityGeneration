931fc416f75c5d5ccc2314d78d4bce2f0dd23190
cizor-v <oscar.cizeron06@gmail.com>
2026-01-01 02:49:29 +0100

    basic L-system
    basic L-system class for road generation, unfinished production rules

---
+++ include/L_system.h
@@ -0,0 +1,32 @@
+#ifndef L_SYSTEM_H
+#define L_SYSTEM_H
+
+#include "turtle.hpp"
+using namespace std;
+
+class L_system
+{
+    public:
+        L_system(string popDensityMap);
+        virtual ~L_system();
+
+        void iterate();
+        void render();
+
+        string getString();
+
+    private:
+        string popDensity_map_path;
+
+        // result string
+        string L_string;
+
+        // functions to initialize / update road parameters
+        vector<vector<string>> globalGoals(string ruleAttr, string roadAttr);
+        string localConstraints(string roadAttr);
+
+        // utility
+        vector<string> splitString(string str, string separator);
+};
+
+#endif // L_SYSTEM_H
---
+++ src/L_system.cpp
@@ -0,0 +1,151 @@
+#include "L_system.h"
+
+L_system::L_system(string popDensityMap)
+{
+    //constructor
+    this.popDensity_map_path = popDensityMap;
+}
+
+L_system::~L_system()
+{
+    //dtor
+}
+
+vector<string> L_system::splitString(string str, string delimiter)
+{
+    vector<string> modules;
+    string module;
+    stringstream ss = stringstream(str);
+
+    while (getline(ss, module, delimiter))
+    {
+        modules.push_back(module);
+    }
+
+    return modules;
+}
+
+vector<vector<string>> L_system::globalGoals(string ruleAttr, string roadAttr)
+{
+    // create parameters for pDel[0-2], pRuleAttr[0-2], pRoadAttr[0-2]
+}
+
+string L_system::localConstraints(string roadAttr)
+{
+    // adjust parameters for state, roadAttr
+}
+
+void L_system::iterate()
+{
+    string temp_L_string = "";
+
+    // remove square brackets
+    string str = this.L_string;
+    str.erase(remove(str.begin(), str.end(), "["), str.end());
+    str.erase(remove(str.begin(), str.end(), "]"), str.end());
+
+    // split L_string into its constituent modules
+    vector<string> modules = splitString(str, ";");
+
+    for (int i = 0; i < modules.size(); i++)
+    {
+        string module = modules.at(i);
+        // apply production rules
+        switch(module[0])
+        {
+        // productions p1, p2, p3
+        case "R":
+            vector<string> params = splitString(splitString(module, ":").at(1), ",");
+
+            // production p1
+            if (std::stoi(params.at(0)) < 0)
+            {
+                break; // do not add a module to the result string
+            }
+
+            if (i+1 < modules.size())
+            {
+                string nextModule = modules.at(i+1);
+                if (nextModule[0] == "I")
+                {
+                    //production p2
+                    string state = splitString(splitString(nextModule, ":").at(1), ",").at[1]
+                    if (state == "SUCCEED")
+                    {
+                        // global goals
+                        break;
+                    }
+
+                    //production p3
+                    if (state == FAILED)
+                    {
+                        break; // do not add a module to the result string
+                    }
+                }
+            }
+            break;
+
+        // productions p4, p5, p6
+        case "B":
+            vector<string> params = splitString(splitString(module, ":").at(1), ",");
+
+            // production p4
+            if (std::stoi(params.at(0)) > 0)
+            {
+                // add B module with same params except del is decremented by 1
+                temp_L_string += "B:" + std::stoi(params.at[0])-1 + "," + params.at(1) + "," + params.at(2) + ";";
+                break;
+            }
+
+            // production p5
+            if (std::stoi(params.at(0)) == 0)
+            {
+                // add branch with R and I modules
+                temp_L_string += "[" +
+                                                    "R:" + params.at(0) + "," + params.at(1) + ";" +
+                                                    "I:" + params.at(2) + "," + "UNASSIGNED" + ";" +
+                                            "]"
+                break;
+            }
+
+            //production p6
+            if (std::stoi(params.at(0)) < 0)
+            {
+                break; // do not add a module to the result string
+            }
+
+            break;
+
+        // productions p7, p8, p9
+        case "I":
+            vector<string> params = splitString(splitString(module, ":").at(1), ",");
+
+            // production p7
+            if (i != 0)
+            {
+                string delPrev = splitString(splitString(modules.at(i-1), ":").at(1), ",").at[0];
+                if (std::stoi(delPrev) < 0)
+                {
+                    break; // do not add a module to the result string
+                }
+            }
+
+            if  (params.at(0) == "UNASSIGNED") // production p8
+            {
+                // local constraints
+                break;
+            }
+            else // production p9
+            {
+                break; // do not add a module to the result string
+            }
+
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    this.L_string = temp_L_string;
+}
